f536dce2dda353102158128a15bcf2cf44c30905
asaulters <71015650+asaulters@users.noreply.github.com>
2021-04-05 21:03:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+// Vars
+let bugName= document.querySelector('#inputName')
+let description = document.querySelector("#inputDescription");
+let severity = document.querySelector("#inputSeverity");
+let responsible = document.querySelector("#inputAssigned");
+let addBtn = document.querySelector('.addBtn');
+let buglogSort = document.querySelector('#bugLogSort');
+let bugLog = document.querySelector('.bugLogLog');
+let bugLogArray = [];
+
+
+//constructors
+
+// function Bug(){
+//     constructor(Bugname, description, severity, responsible) {
+//         this.Bugname = Bugname;
+//         this.description = description;     
+//         this.severity = severity;
+//         this.responsible = responsible;
+//         this.status = "open";
+//     }
+// }
+
+function Bug(bugName, description, severity, responsible, status){
+        this.bugName = bugName;
+        this.description = description;     
+        this.severity = severity;
+        this.responsible = responsible;
+        this.status = "open";
+    }
+
+
+
+
+//functions
+
+function addBug(e){
+    e.preventDefault();
+    //take input
+    let des = description.value;
+    let sev = severity.value
+    let resp = responsible.value;
+    let newBugName = bugName.value;
+    let bugStatus;
+    let newBugItem = new Bug(newBugName, des, sev, resp, bugStatus);
+    
+
+    //New Div
+    const bugDiv = document.createElement("div");
+    bugDiv.classList.add('bugListDiv');
+    //create prop divs
+    const bugNameDiv = document.createElement("div");
+    bugNameDiv.classList.add('bugNameDiv');
+    const bugDesDiv = document.createElement("div");
+    bugDesDiv.classList.add('bugDesDiv');
+    const bugSevDiv = document.createElement("div");
+    bugSevDiv.classList.add('bugSevDiv');
+    const bugRespDiv = document.createElement("div");
+    bugRespDiv.classList.add('bugRespDiv');
+    const bugStatusDiv = document.createElement("div");
+    bugStatusDiv.classList.add('bugStatusDiv');
+    const closeBugButton = document.createElement("button");
+    closeBugButton.classList.add('closeBugButton');
+    const deleteBugButton = document.createElement("button");
+    deleteBugButton.classList.add('deleteBugButton');
+    //Values to div
+    bugNameDiv.innerText = newBugItem.bugName;
+    bugDesDiv.innerText = newBugItem.description;
+    bugSevDiv.innerText = newBugItem.severity;
+    bugRespDiv.innerText = newBugItem.responsible;
+    newBugItem.bugStatus="Open";
+    bugStatusDiv.innerText = newBugItem.bugStatus;
+    console.log(bugStatusDiv)
+    closeBugButton.innerText = "Close";
+    deleteBugButton.innerText = "Delete";
+    
+
+    //create New Bug Div
+    const newBug = document.createElement("div");
+    newBug.appendChild(bugNameDiv);
+    newBug.appendChild(bugDesDiv);
+    newBug.appendChild(bugSevDiv);
+    newBug.appendChild(bugRespDiv);
+    newBug.appendChild(bugStatusDiv);
+    newBug.appendChild(closeBugButton);
+    newBug.appendChild(deleteBugButton);
+    newBug.classList.add('bug-item');
+    
+
+    //NewbugList
+    const NewbugList = document.createElement("ul");
+    NewbugList.classList.add('bug-list');
+    NewbugList.appendChild(newBug);
+
+    //append new bug to element
+    bugDiv.appendChild(NewbugList);
+    bugLog.appendChild(bugDiv);
+    // reset form
+    description.value = "";
+    severity.value= "Choose Severity";
+    responsible.value= "";
+    bugName.value= "";
+}
+
+addBtn.addEventListener('click', addBug);
